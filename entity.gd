0fe52089a5c4f12e44c623e9ec277cf50b09e7ce
Jack0nail <Jack0nail@yandex.ru>
2025-11-03 21:23:20 +0300

    turn improvment

--- entity.gd
+++ entity.gd
@@ -3,9 +3,8 @@ extends Node
 class_name Entity
 
 var pos: Vector2i
+var ocherednum: int
 
-
-func _init(coord: Vector2i) -> void:
+func _init(coord: Vector2i, ochered: int) -> void:
 	pos = coord
-
-	
+	ocherednum = ochered
--- mainnode.gd
+++ mainnode.gd
@@ -8,25 +8,38 @@ extends Node
 
 var player_entity 
 var monster_entity
+var ochered_xod
+var players
+var player
+var turn_order
+var turn_index: int
+var round: int
+var js_path = "res://player_monster.json"
 
 func _ready() -> void:
 	
 	for y in 10:
 		for x in 10:
 			terrain_map_layer.set_cell(Vector2i(x, y), 0, Vector2i(randi() % 18, 0))
-	monster_entity = Entity.new(Vector2i(randi()%8+1,randi()%8+1))
+	var file = FileAccess.open(js_path, FileAccess.READ)
+	var js_string = file.get_as_text()
+	var data = JSON.parse_string(js_string)
+	var turn = data["turn"]
+	turn_order = turn["order"]
+	turn_index = turn["currentIndex"]
+	round = turn["round"]
+	players = data["players"]
+	monster_entity = Entity.new(Vector2i(randi()%8+1,randi()%8+1),4)
 	monster_map_layer.set_cell(monster_entity.pos, 0, Vector2i(0, 0))
-	player_map_layer.set_cell(Vector2i(0, 0), 1, Vector2i(0, 0))
-	player_entity = Entity.new(player_map_layer.get_cell_atlas_coords(Vector2i(0,0)))
-	player_map_layer.set_cell(Vector2i(0, 9), 1, Vector2i(0, 0))
-	player_entity = Entity.new(player_map_layer.get_cell_atlas_coords(Vector2i(0,9)))
-	player_map_layer.set_cell(Vector2i(9, 0), 1, Vector2i(0, 0))
-	player_entity = Entity.new(player_map_layer.get_cell_atlas_coords(Vector2i(9,0)))
-	player_map_layer.set_cell(Vector2i(9, 9), 1, Vector2i(0, 0))
-	player_entity = Entity.new(player_map_layer.get_cell_atlas_coords(Vector2i(9,9)))
+	player_map_layer.set_cell(Vector2i(players["P1"]["pos"]["x"],players["P1"]["pos"]["y"]), 1, Vector2i(0, 0))
+	player_entity = Entity.new(player_map_layer.get_cell_atlas_coords(Vector2i(0,0)),0)
+	player_map_layer.set_cell(Vector2i(players["P2"]["pos"]["x"],players["P2"]["pos"]["y"]), 1, Vector2i(0, 0))
+	player_entity = Entity.new(player_map_layer.get_cell_atlas_coords(Vector2i(0,9)),1)
+	player_map_layer.set_cell(Vector2i(players["P3"]["pos"]["x"],players["P3"]["pos"]["y"]), 1, Vector2i(0, 0))
+	player_entity = Entity.new(player_map_layer.get_cell_atlas_coords(Vector2i(9,0)),2)
+	player_map_layer.set_cell(Vector2i(players["P4"]["pos"]["x"],players["P4"]["pos"]["y"]), 1, Vector2i(0, 0))
+	player_entity = Entity.new(player_map_layer.get_cell_atlas_coords(Vector2i(9,9)),3)
 	monster_map_layer.hide()
-
-	
 	
 	for y in 10:
 		for x in 10:
@@ -68,52 +81,106 @@ func _on_button_2_pressed() -> void:
 
 func _rannum():
 	if (randi()%100+1) <= 20:
-		$BoxContainer.show()
-		
+		pass#$BoxContainer.show()
+
 	var numran = randi()%10 
 	match numran:
 		0:
-			$BoxContainer/head.set("text","test 0")
-			$BoxContainer/bodytext.set("text","ya yebal no ya smog")
+			$BoxContainer/Control3/head.set("text","Ивент 1:")
+			$BoxContainer/Control3/bodyText.set("text","Вам на пути попался бродячий торговец. Он заявляет что он успешный зельевар, хоть вы о нем никогда не слышали. Он предлагает опробовать вам свой новый \"эликсир\", дающий \"прозрение\". Он назвал его \"Кубок огня\", а на этикетке нарисовано 3 острых перца. Примите его предложение?")
+			$BoxContainer/Control2/Button.set("text","\"Да, почему нет?\"")
+			$BoxContainer/Control2/Button2.set("text","Нет, лучше не доверять прохожим")
+			
 		1:
-			$BoxContainer/head.set("text","test 1")
-			$BoxContainer/bodytext.set("text","ya yebal no ya smog")
+			$BoxContainer/Control3/head.set("text","Ивент 2:")
+			$BoxContainer/Control3/bodyText.set("text","По мере вашего продвижения у вас на пути оказывается завал. Обходить его придётся долго, проходить через него выйдет проблематично.")
+			$BoxContainer/Control2/Button.set("text","О как")
+			$BoxContainer/Control2/Button2.hide()
+			$BoxContainer/Control2/Button2.set("text"," ")
 		2:
-			$BoxContainer/head.set("text","test 2")
-			$BoxContainer/bodytext.set("text","ya yebal no ya smog")
+			$BoxContainer/Control3/head.set("text","Ивент 3:")
+			$BoxContainer/Control3/bodyText.set("text","Вам встретился старик в чёрной робе, стоящий на мосту. Мост расположен над пропастью, именуемой \"Ущелье Вечной Опасности\", если верить табличке рядом с ней. Туман скрывает дно, не позволяя понять насколько глубокое это ущелье. \"Вщяк щуда идущий, отвещь на вопрощ и можещь идти. Ответищь неправильно - умрёщь. Какова скорощть лащточки бещ груза?\"")
+			$BoxContainer/Control2/Button.set("text","Щто?")
+			$BoxContainer/Control2/Button2.set("text","0.050283 маха")
 		3:
-			$BoxContainer/head.set("text","test 3")
-			$BoxContainer/bodytext.set("text","ya yebal no ya smog")
+			$BoxContainer/Control3/head.set("text","Ивент 4:")
+			$BoxContainer/Control3/bodyText.set("text","Вы нашли поляну полную сочных, ярких и красочных ягод. Вы решили сорвать немного себе в запас. Быть может и варенье сварите.")
+			$BoxContainer/Control2/Button.set("text","О как")
+			$BoxContainer/Control2/Button2.hide()
+			$BoxContainer/Control2/Button2.set("text"," ")
 		4:
-			$BoxContainer/head.set("text","test 4")
-			$BoxContainer/bodytext.set("text","ya yebal no ya smog")
+			$BoxContainer/Control3/head.set("text","test 4")
+			$BoxContainer/Control3/bodyText.set("text","У вас на пути попался бродячий торговец. Он заявляет что он успешный зельевар, хоть вы о нем никогда не слышали. Он предлагает опробовать вам свой новый эликсир, дающий прозрение. Он назвал его Кубок огня, а на этикетке нарисовано 3 острых перца, сложенные в букву Щ. Примите его предложение?")
+			$BoxContainer/Control2/Button.set("text","\"Да, почему нет?\" (На 2-3 хода получаешь возможность видеть дальше на 1 клетку, но другие игроки тут же узнают о твоём местоположении) \"Этот напиток разливается по вашему телу, согревая его своей остротой. Ваши рецепторы тут же взорвались, лицо покрылось испаринами а тело, словно ракета, подлетело в небо, открывая вам виды вокруг. По приземлении, торговца вы уже не обнаружили, но вы уверены, что вы не остались незамеченными.\"")
+			$BoxContainer/Control2/Button2.set("text","Нет, лучше не доверять прохожим.\" (ничего не меняется) \"Вы пошли своим путём дальше, оставляя торговца позади")
 		5:
-			$BoxContainer/head.set("text","test 5")
-			$BoxContainer/bodytext.set("text","ya yebal no ya smog")
+			$BoxContainer/Control3/head.set("text","test 5")
+			$BoxContainer/Control3/bodyText.set("text","У вас на пути попался бродячий торговец. Он заявляет что он успешный зельевар, хоть вы о нем никогда не слышали. Он предлагает опробовать вам свой новый эликсир, дающий прозрение. Он назвал его Кубок огня, а на этикетке нарисовано 3 острых перца, сложенные в букву Щ. Примите его предложение?")
+			$BoxContainer/Control2/Button.set("text","\"Да, почему нет?\" (На 2-3 хода получаешь возможность видеть дальше на 1 клетку, но другие игроки тут же узнают о твоём местоположении) \"Этот напиток разливается по вашему телу, согревая его своей остротой. Ваши рецепторы тут же взорвались, лицо покрылось испаринами а тело, словно ракета, подлетело в небо, открывая вам виды вокруг. По приземлении, торговца вы уже не обнаружили, но вы уверены, что вы не остались незамеченными.\"")
+			$BoxContainer/Control2/Button2.set("text","Нет, лучше не доверять прохожим.\" (ничего не меняется) \"Вы пошли своим путём дальше, оставляя торговца позади")
 		6:
-			$BoxContainer/head.set("text","test 6")
-			$BoxContainer/bodytext.set("text","ya yebal no ya smog")
+			$BoxContainer/Control3/head.set("text","test 6")
+			$BoxContainer/Control3/bodyText.set("text","У вас на пути попался бродячий торговец. Он заявляет что он успешный зельевар, хоть вы о нем никогда не слышали. Он предлагает опробовать вам свой новый эликсир, дающий прозрение. Он назвал его Кубок огня, а на этикетке нарисовано 3 острых перца, сложенные в букву Щ. Примите его предложение?")
+			$BoxContainer/Control2/Button.set("text","\"Да, почему нет?\" (На 2-3 хода получаешь возможность видеть дальше на 1 клетку, но другие игроки тут же узнают о твоём местоположении) \"Этот напиток разливается по вашему телу, согревая его своей остротой. Ваши рецепторы тут же взорвались, лицо покрылось испаринами а тело, словно ракета, подлетело в небо, открывая вам виды вокруг. По приземлении, торговца вы уже не обнаружили, но вы уверены, что вы не остались незамеченными.\"")
+			$BoxContainer/Control2/Button2.set("text","Нет, лучше не доверять прохожим.\" (ничего не меняется) \"Вы пошли своим путём дальше, оставляя торговца позади")
 		7:
-			$BoxContainer/head.set("text","test 7")
-			$BoxContainer/bodytext.set("text","ya yebal no ya smog")
+			$BoxContainer/Control3/head.set("text","test 7")
+			$BoxContainer/Control3/bodyText.set("text","У вас на пути попался бродячий торговец. Он заявляет что он успешный зельевар, хоть вы о нем никогда не слышали. Он предлагает опробовать вам свой новый эликсир, дающий прозрение. Он назвал его Кубок огня, а на этикетке нарисовано 3 острых перца, сложенные в букву Щ. Примите его предложение?")
+			$BoxContainer/Control2/Button.set("text","\"Да, почему нет?\" (На 2-3 хода получаешь возможность видеть дальше на 1 клетку, но другие игроки тут же узнают о твоём местоположении) \"Этот напиток разливается по вашему телу, согревая его своей остротой. Ваши рецепторы тут же взорвались, лицо покрылось испаринами а тело, словно ракета, подлетело в небо, открывая вам виды вокруг. По приземлении, торговца вы уже не обнаружили, но вы уверены, что вы не остались незамеченными.\"")
+			$BoxContainer/Control2/Button2.set("text","Нет, лучше не доверять прохожим.\" (ничего не меняется) \"Вы пошли своим путём дальше, оставляя торговца позади")
 		8:
-			$BoxContainer/head.set("text","test 8")
-			$BoxContainer/bodytext.set("text","ya yebal no ya smog")
+			$BoxContainer/Control3/head.set("text","test 8")
+			$BoxContainer/Control3/bodyText.set("text","У вас на пути попался бродячий торговец. Он заявляет что он успешный зельевар, хоть вы о нем никогда не слышали. Он предлагает опробовать вам свой новый эликсир, дающий прозрение. Он назвал его Кубок огня, а на этикетке нарисовано 3 острых перца, сложенные в букву Щ. Примите его предложение?")
+			$BoxContainer/Control2/Button.set("text","\"Да, почему нет?\" (На 2-3 хода получаешь возможность видеть дальше на 1 клетку, но другие игроки тут же узнают о твоём местоположении) \"Этот напиток разливается по вашему телу, согревая его своей остротой. Ваши рецепторы тут же взорвались, лицо покрылось испаринами а тело, словно ракета, подлетело в небо, открывая вам виды вокруг. По приземлении, торговца вы уже не обнаружили, но вы уверены, что вы не остались незамеченными.\"")
+			$BoxContainer/Control2/Button2.set("text","Нет, лучше не доверять прохожим.\" (ничего не меняется) \"Вы пошли своим путём дальше, оставляя торговца позади")
 		9:
-			$BoxContainer/head.set("text","test 9")
-			$BoxContainer/bodytext.set("text","ya yebal no ya smog")
-			$BoxContainer/Control2/Button.set("text","sosal")
-			$BoxContainer/Control2/Button2.set("text","ebali")
+			$BoxContainer/Control3/head.set("text","test 9")
+			$BoxContainer/Control3/bodyText.set("text","У вас на пути попался бродячий торговец. Он заявляет что он успешный зельевар, хоть вы о нем никогда не слышали. Он предлагает опробовать вам свой новый эликсир, дающий прозрение. Он назвал его Кубок огня, а на этикетке нарисовано 3 острых перца, сложенные в букву Щ. Примите его предложение?")
+			$BoxContainer/Control2/Button.set("text","\"Да, почему нет?\" (На 2-3 хода получаешь возможность видеть дальше на 1 клетку, но другие игроки тут же узнают о твоём местоположении) \"Этот напиток разливается по вашему телу, согревая его своей остротой. Ваши рецепторы тут же взорвались, лицо покрылось испаринами а тело, словно ракета, подлетело в небо, открывая вам виды вокруг. По приземлении, торговца вы уже не обнаружили, но вы уверены, что вы не остались незамеченными.\"")
+			$BoxContainer/Control2/Button2.set("text","Нет, лучше не доверять прохожим.\" (ничего не меняется) \"Вы пошли своим путём дальше, оставляя торговца позади")
+
 func move_player(x:int, y:int) -> void:
-	if (player_entity.pos.x+x<10 && player_entity.pos.x+x>-1 && player_entity.pos.y+y<10 && player_entity.pos.y+y>-1):
-		player_map_layer.erase_cell(player_entity.pos)
-		player_entity.pos.x += x
-		player_entity.pos.y += y
-		player_map_layer.set_cell(player_entity.pos, 1, Vector2i(0, 0))
-		fart_map_layer.erase_cell(player_entity.pos)
-		#_rannum()
-		move_monster()
-		monster_brain()
+	if turn_index <= 3:
+		match turn_index:
+			0:
+				if (int(players["P1"]["pos"]["x"])+x<10 && int(players["P1"]["pos"]["x"])+x>-1 && int(players["P1"]["pos"]["y"])+y<10 && int(players["P1"]["pos"]["y"])+y>-1):
+					player_map_layer.erase_cell(Vector2i(players["P1"]["pos"]["x"],players["P1"]["pos"]["y"]))
+					players["P1"]["pos"]["x"] += x
+					players["P1"]["pos"]["y"] += y
+					player_map_layer.set_cell(Vector2i(players["P1"]["pos"]["x"],players["P1"]["pos"]["y"]), 1, Vector2i(0, 0))
+					fart_map_layer.erase_cell(Vector2i(players["P1"]["pos"]["x"],players["P1"]["pos"]["y"]))
+					_rannum()
+					turn_index += 1
+			1:
+				if (int(players["P2"]["pos"]["x"])+x<10 && int(players["P2"]["pos"]["x"])+x>-1 && int(players["P2"]["pos"]["y"])+y<10 && int(players["P2"]["pos"]["y"])+y>-1):
+					player_map_layer.erase_cell(Vector2i(players["P2"]["pos"]["x"],players["P2"]["pos"]["y"]))
+					players["P2"]["pos"]["x"] += x
+					players["P2"]["pos"]["y"] += y
+					player_map_layer.set_cell(Vector2i(players["P2"]["pos"]["x"],players["P2"]["pos"]["y"]), 1, Vector2i(0, 0))
+					fart_map_layer.erase_cell(Vector2i(players["P2"]["pos"]["x"],players["P2"]["pos"]["y"]))
+					_rannum()
+					turn_index += 1
+			2:
+				if (int(players["P3"]["pos"]["x"])+x<10 && int(players["P3"]["pos"]["x"])+x>-1 && int(players["P3"]["pos"]["y"])+y<10 && int(players["P3"]["pos"]["y"])+y>-1):
+					player_map_layer.erase_cell(Vector2i(players["P3"]["pos"]["x"],players["P3"]["pos"]["y"]))
+					players["P3"]["pos"]["x"] += x
+					players["P3"]["pos"]["y"] += y
+					player_map_layer.set_cell(Vector2i(players["P3"]["pos"]["x"],players["P3"]["pos"]["y"]), 1, Vector2i(0, 0))
+					fart_map_layer.erase_cell(Vector2i(players["P3"]["pos"]["x"],players["P3"]["pos"]["y"]))
+					_rannum()
+					turn_index += 1
+			3:
+				if (int(players["P4"]["pos"]["x"])+x<10 && int(players["P4"]["pos"]["x"])+x>-1 && int(players["P4"]["pos"]["y"])+y<10 && int(players["P4"]["pos"]["y"])+y>-1):
+					player_map_layer.erase_cell(Vector2i(players["P4"]["pos"]["x"],players["P4"]["pos"]["y"]))
+					players["P4"]["pos"]["x"] += x
+					players["P4"]["pos"]["y"] += y
+					player_map_layer.set_cell(Vector2i(players["P4"]["pos"]["x"],players["P4"]["pos"]["y"]), 1, Vector2i(0, 0))
+					fart_map_layer.erase_cell(Vector2i(players["P4"]["pos"]["x"],players["P4"]["pos"]["y"]))
+					_rannum()
+					turn_index += 1
+	else: 
+		turn_index = 0
+		round +=1
 		
 func move_monster():
 	var x = randi()%3-1
@@ -124,6 +191,7 @@ func move_monster():
 			monster_entity.pos.y +=y
 			monster_map_layer.set_cell(monster_entity.pos, 0, Vector2i(0, 0))
 	else: move_monster()
+	
 func monster_brain():
 	monster_hide()
 	
@@ -131,6 +199,4 @@ func monster_hide():
 	if (monster_entity.pos.x - player_entity.pos.x >=-1 && monster_entity.pos.x - player_entity.pos.x <=1 && monster_entity.pos.y - player_entity.pos.y >=-1 && monster_entity.pos.y - player_entity.pos.y <=1):
 		monster_map_layer.show()
 	else: monster_map_layer.hide()
-func monster_chase():
-	if (monster_entity.pos.x - player_entity.pos.x >=-1 && monster_entity.pos.x - player_entity.pos.x <=1 && monster_entity.pos.y - player_entity.pos.y >=-1 && monster_entity.pos.y - player_entity.pos.y <=1):
-		player_entity.pos
+	
