b75ee05cacb4396d43cb0556c80de202a9200499
VampireCraft <61780197+VampireCraft@users.noreply.github.com>
2025-11-04 15:50:45 +0300

    pisun2

--- mainnode.gd
+++ mainnode.gd
@@ -3,15 +3,9 @@ extends Node
 @onready var terrain_map_layer: TileMapLayer = $TerrainMap
 @onready var object_map_layer: TileMapLayer = $ObjectMap
 @onready var fart_map_layer: TileMapLayer = $FartMap
-@onready var monster_map_layer: TileMapLayer = $MonsterMap
 @onready var border_map_layer: TileMapLayer = $BorderMap
-@onready var player_map_layer: TileMapLayer = $PlayerMap
 @onready var player_on_map = $Playerr
 
-#var xp = 0
-#var yp = 0
-var player
-var monster
 var xmap = 0
 var ymap = 0
 var cell_type_map: Dictionary[Vector2i,String]
@@ -19,25 +13,8 @@ var is_move: bool = true
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	#for y in 15:
-		#for x in 15:
-			#terrain_map_layer.set_cell(Vector2i(x, y), 2, Vector2i(randi() % 21, 0))
-	#player_map_layer.set_cell(Vector2i(0, 0), 1, Vector2i(0, 0))
-	##player_map_layer.set_cell(Vector2i(0, 9), 1, Vector2i(0, 0))
-	##player_map_layer.set_cell(Vector2i(9, 0), 1, Vector2i(0, 0))
-	##player_map_layer.set_cell(Vector2i(9, 9), 1, Vector2i(0, 0))
-	#
-	#
-	#for y in 10:
-		#for x in 10:
-			#if (player_map_layer.get_cell_source_id(Vector2i(x, y)) == -1):
-				#if (x == 9 || y == 9):
-					#fart_map_layer.set_cell(Vector2i(x, y), 0, Vector2i(0, 0))
-				#else:
-					#fart_map_layer.set_cell(Vector2i(x, y), 1, Vector2i(0, 0))
 	$HTTPRequest2.request_completed.connect(_on_request_completed)
 	$HTTPRequest2.request("https://peak.e-dt.ru/maps/get_map/2")
-	
 				
 #func _on_request_id_completed(result, response_code, headers, body):
 	#$HTTPRequest.request_completed.connect(_on_request_completed)
@@ -49,9 +26,6 @@ func _on_request_completed(result, response_code, headers, body):
 	var json = JSON.parse_string(body.get_string_from_utf8())
 	xmap = json["size"]["width"]
 	ymap = json["size"]["height"]
-	player = Entity.new(Vector2i(0,0))
-	player_map_layer.set_cell(player.pos, 1, Vector2i(2, 0))
-	border_map_layer.set_cell(player.pos, 0, Vector2i(0, 0))
 	for i in json["cells"]:
 		var a
 		cell_type_map.set(Vector2i(i["x"],i["y"]),i["cell_type"])
@@ -61,13 +35,16 @@ func _on_request_completed(result, response_code, headers, body):
 			a = 1
 		if (i["cell_items"].size() != 0):
 			if (i["cell_items"][0] == "ANGRY_PERSON"):
-				monster = Entity.new(Vector2i(i["x"],i["y"]))
+				#monster = Entity.new(Vector2i(i["x"],i["y"]))
+				pass
 		terrain_map_layer.set_cell(Vector2i(i["x"], i["y"]), 3, Vector2i(a, 0))
-		if (player_map_layer.get_cell_source_id(Vector2i(i["x"], i["y"])) == -1):
+		if (player_on_map.pos == Vector2i(i["x"], i["y"])):
 			if (i["x"] == xmap-1 || i["y"] == ymap-1):
 				fart_map_layer.set_cell(Vector2i(i["x"], i["y"]), 2, Vector2i(3, 0))
 			else:
 				fart_map_layer.set_cell(Vector2i(i["x"], i["y"]), 2, Vector2i(3, 0))
+	fart_map_layer.erase_cell(Vector2i.ZERO)
+	reset_border(player_on_map.pos)
 	
 	
 func _on_button_up_pressed() -> void:
@@ -81,28 +58,8 @@ func _on_button_right_pressed() -> void:
 	
 func _on_button_down_pressed() -> void:
 	move_player(1,0)
-
-#func _on_button_up_right_pressed() -> void:
-	#move_player(-1,-1)
-#
-#func _on_button_right_up_pressed() -> void:
-	#move_player(1,-1)
-#
-#func _on_button_down_left_pressed() -> void:
-	#move_player(1,1)
-#
-#func _on_button_left_down_pressed() -> void:
-	#move_player(-1,1)
 	
 func move_player(x:int, y:int) -> void:
-	if (player.pos.x+x<10 && player.pos.x+x>-1 && player.pos.y+y<10 && player.pos.y+y>-1):
-		player_map_layer.erase_cell(player.pos)
-		border_map_layer.erase_cell(player.pos)
-		player.pos.x += x
-		player.pos.y += y
-		player_map_layer.set_cell(player.pos, 1, Vector2i(0, 0))
-		border_map_layer.set_cell(player.pos, 0)
-		fart_map_layer.erase_cell(player.pos)
 		#_rannum()
 		move_monster()
 		monster_brain()
@@ -110,68 +67,46 @@ func move_player(x:int, y:int) -> void:
 func move_monster():
 	var x = randi()%3-1
 	var y = randi()%3-1  
-	if (monster.pos.x+x<10 && monster.pos.x+x>-1 && monster.pos.y+y<10 && monster.pos.y+y>-1):
-			monster_map_layer.erase_cell(monster.pos)
-			monster.pos.x +=x
-			monster.pos.y +=y
-			monster_map_layer.set_cell(monster.pos, 0, Vector2i(0, 0))
-	else: move_monster()
+	#if (monster.pos.x+x<10 && monster.pos.x+x>-1 && monster.pos.y+y<10 && monster.pos.y+y>-1):
+			#monster_map_layer.erase_cell(monster.pos)
+			#monster.pos.x +=x
+			#monster.pos.y +=y
+			#monster_map_layer.set_cell(monster.pos, 0, Vector2i(0, 0))
+	#else: move_monster()
+	
 func monster_brain():
 	monster_hide()
 	
 func monster_hide():
-	if (monster.pos.x - player.pos.x >=-1 && monster.pos.x - player.pos.x <=1 && monster.pos.y - player.pos.y >=-1 && monster.pos.y - player.pos.y <=1):
-		monster_map_layer.show()
-	else: monster_map_layer.hide()
+	#if (monster.pos.x - player.pos.x >=-1 && monster.pos.x - player.pos.x <=1 && monster.pos.y - player.pos.y >=-1 && monster.pos.y - player.pos.y <=1):
+		#monster_map_layer.show()
+	#else: monster_map_layer.hide()
+	pass
 func monster_chase():
-	if (monster.pos.x - player.pos.x >=-1 && monster.pos.x - player.pos.x <=1 && monster.pos.y - player.pos.y >=-1 && monster.pos.y - player.pos.y <=1):
-		player.pos
+	#if (monster.pos.x - player.pos.x >=-1 && monster.pos.x - player.pos.x <=1 && monster.pos.y - player.pos.y >=-1 && monster.pos.y - player.pos.y <=1):
+		#player.pos
+		pass
 
-
-#func move_player(x:int, y:int) -> void:
-	#if (player.pos.x+x<10 || player.pos.x+x>-1 || player.pos.y+y<10 || player.pos.y+y>-1):
-		#player_map_layer.erase_cell(player.pos)
-		#player.pos.x += x
-		#player.pos.y += y
-		#player_map_layer.set_cell(player.pos, 1, Vector2i(0, 0))
-		#fart_map_layer.erase_cell(player.pos)
-		#func _on_button_up_pressed() -> void:
-  #if (player.pos.x-1>-1):
-	#player_map_layer.erase_cell(player.pos)
-	#player.pos.x -= 1
-	#player_map_layer.set_cell(player.pos,1,Vector2i(0,0))
-	#fart_map_layer.erase_cell(player.pos)
-
-#func _on_button_up_pressed() -> void:
-	#if (xp-1>-1):
-		#player_map_layer.erase_cell(Vector2i(xp, yp))
-		#xp-=1
-		#player_map_layer.set_cell(Vector2i(xp, yp), 1, Vector2i(0, 0))
-		#fart_map_layer.erase_cell(Vector2i(xp, yp))
-	#
-#func _on_button_left_pressed() -> void:
-	#if (yp+1<ymap):
-		#player_map_layer.erase_cell(Vector2i(xp, yp))
-		#yp+=1
-		#player_map_layer.set_cell(Vector2i(xp, yp), 1, Vector2i(0, 0))
-		#fart_map_layer.erase_cell(Vector2i(xp, yp))
-		#
-	#
-#func _on_button_right_pressed() -> void:
-	#if (yp-1>-1):
-		#player_map_layer.erase_cell(Vector2i(xp, yp))
-		#yp-=1
-		#player_map_layer.set_cell(Vector2i(xp, yp), 1, Vector2i(0, 0))
-		#fart_map_layer.erase_cell(Vector2i(xp, yp))
-	#
-#func _on_button_down_pressed() -> void:
-	#if (xp+1<xmap):
-		#player_map_layer.erase_cell(Vector2i(xp, yp))
-		#xp+=1
-		#player_map_layer.set_cell(Vector2i(xp, yp), 1, Vector2i(0, 0))
-		#fart_map_layer.erase_cell(Vector2i(xp, yp))
-
-
+func reset_border(coodr: Vector2i) -> void:
+	border_map_layer.clear()
+	border_map_layer.set_cell(coodr, 0, Vector2i(1,0))
+	if player_on_map.pos.x + 1 < xmap:
+		var pos = coodr
+		pos.x+=1
+		border_map_layer.set_cell(pos, 0, Vector2i(1,3))
+	if player_on_map.pos.x - 1 > -1:
+		var pos = coodr
+		pos.x-=1
+		border_map_layer.set_cell(pos, 0, Vector2i(1,3))
+	if player_on_map.pos.y + 1 < ymap:
+		var pos = coodr
+		pos.y+=1
+		border_map_layer.set_cell(pos, 0, Vector2i(1,3))
+	if player_on_map.pos.y - 1 > -1:
+		var pos = coodr
+		pos.y-=1
+		border_map_layer.set_cell(pos, 0, Vector2i(1,3))
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if is_move:
@@ -179,11 +114,14 @@ func _process(delta: float) -> void:
 			var cell_cord =terrain_map_layer.local_to_map(terrain_map_layer.get_local_mouse_position())
 			print(cell_cord)
 			var mod = cell_cord-player_on_map.pos
-			print(player_on_map.pos+mod)
-			if (abs(mod.x)+abs(mod.y)<2):
-				if (player_on_map.pos + mod >= Vector2i.ZERO && player_on_map.pos + mod < Vector2i(xmap, ymap)):
+			#print(player_on_map.pos+mod)
+			if (abs(mod.x)+abs(mod.y)==1):
+				if (cell_cord.x >-1 && cell_cord.x < xmap && cell_cord.y >-1 && cell_cord.y < ymap):
 					player_on_map.pos = cell_cord
+					fart_map_layer.erase_cell(cell_cord)
+					reset_border(cell_cord)
 					var cell_pos_global = terrain_map_layer.to_global(terrain_map_layer.map_to_local(cell_cord))
 					#print(cell_pos_global)
+					print("hui")
 					cell_pos_global.y -= 15
 					player_on_map.move(Vector2(float(cell_pos_global.x), float(cell_pos_global.y)))
