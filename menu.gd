55f09e283892b00ea7b8fd74b09f663c660885b1
Jack0nail <Jack0nail@yandex.ru>
2025-11-04 14:55:02 +0300

    menu_update

--- menu.gd
+++ menu.gd
@@ -1,9 +1,9 @@
 extends Node2D
 
 
-func _on_exit_pressed() -> void:
-	get_tree().quit()
-
-
 func _on_play_pressed() -> void:
 	get_tree().change_scene_to_file("res://node.tscn")
+
+
+func _on_button_sh_pressed() -> void:
+	pass # Replace with function body.
